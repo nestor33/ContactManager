fd27c32effaf493205cce2dea024cc4d4a2f76a7
Nestor Pylat <nestorpylat33@gmail.com>
2021-12-11 12:18:54 +0200

    setup

---
+++ ContactManager/Controllers/FileUploadController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Threading.Tasks;
+
+namespace ReactProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileUploadController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> OnPostUploadAsync(IFormFile file)
+        {
+            if (file is null)
+            {
+                throw new ArgumentNullException(nameof(file));
+            }
+            using (TextFieldParser parser = new TextFieldParser(file.OpenReadStream()))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    //Process row
+                    string[] fields = parser.ReadFields();
+                    foreach (string field in fields)
+                    {
+                        //TODO: Process field
+                    }
+                }
+            }
+
+            return Ok(new { file});
+        }
+    }
+}
